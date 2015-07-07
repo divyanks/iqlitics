d4198c5f0b72d1c6ec3aa96521c7e9aa4910ddda
U-Lenovo-PC\LENOVO <you@example.com>
2015-07-07 16:25:39 +0530

    Adding config panel

--- app/client/app.js
+++ app/client/app.js
@@ -1,11 +1,66 @@
 
 
-var app = angular.module('app', ['ui.router', 'ngSanitize', 'ngCookies', 'DonutModule', 'BarModule', 'LineModule']);
+var app = angular.module('app', ['ui.router','ui.bootstrap', 'ngSanitize', 'ngCookies', 'DonutModule', 'BarModule', 'LineModule']);
 
 var apiFactory = require('./factories/api');
 
 app.controller('donutController', function($scope , donutProvider){    
 	donutProvider.createDonut();
+});
+
+app.controller('modalSettingsCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+        transclude: true,
+        restrict: 'E',
+      templateUrl: '/assets/views/partials/configForm.hbs',
+      controller: 'ModalInstanceCtrl',
+        
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
 
 app.controller('barController', function($scope , barProvider){    
